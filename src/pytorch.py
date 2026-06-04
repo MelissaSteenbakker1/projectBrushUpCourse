d06c5605e3111e7a92fd0af638d9c0880f849be3
keukenroller <kairook22@gmail.com>
2026-06-04 19:01:55 +0200

    Check GPU availability

--- src/pytorch.py
+++ src/pytorch.py
@@ -1,2 +1,8 @@
 import torch
 print("Using torch", torch.__version__)
+
+gpu_avail = torch.cuda.is_available()
+print(f"Is the GPU available? {gpu_avail}")
+
+device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+print("Device", device)
