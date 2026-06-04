43ec47fc1a6e3ac5b65785df9ea01e7a8853613f
ML Steenbakker <melissa.steenbakker@student.uva.nl>
2026-06-04 11:23:17 +0200

    Update import.py

--- import.py
+++ import.py
@@ -1,28 +1,42 @@
 import medmnist
-import torch.utils.data
 from medmnist import INFO
 from torch.utils.data import DataLoader
+from torchvision import transforms
 
 data_flag = 'chestmnist'
 
 info = INFO[data_flag]
 DataClass = getattr(medmnist, info['python_class'])
 
-# Dataset laden
-train_dataset = DataClass(split='train', download=True)
-val_dataset = DataClass(split='val', download=True)
-test_dataset = DataClass(split='test', download=True)
+transform = transforms.ToTensor()
 
-# DataLoaders
+train_dataset = DataClass(
+    split='train',
+    transform=transform,
+    download=True
+)
+
+val_dataset = DataClass(
+    split='val',
+    transform=transform,
+    download=True
+)
+
+test_dataset = DataClass(
+    split='test',
+    transform=transform,
+    download=True
+)
+
 train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True)
 val_loader = DataLoader(val_dataset, batch_size=64, shuffle=False)
 test_loader = DataLoader(test_dataset, batch_size=64, shuffle=False)
 
-# Print dataset
 print("Train samples:", len(train_dataset))
 print("Validation samples:", len(val_dataset))
 print("Test samples:", len(test_dataset))
 
 images, labels = next(iter(train_loader))
+
 print("Image batch shape:", images.shape)
 print("Label batch shape:", labels.shape)
