6e181fc4e420a293054e76a6c7246060765f0c16
keukenroller <kairook22@gmail.com>
2026-06-04 19:01:37 +0200

    Created model training and evaluation script

--- src/train.py
+++ src/train.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 from model import ChestMNISTModel
 from importdata import get_chestmnist_loaders
 
-def train(num_epochs=10, batch_size=64, learning_rate=0.001):
+def train(num_epochs=20, batch_size=64, learning_rate=0.001):
 
     # Use GPU if available
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
